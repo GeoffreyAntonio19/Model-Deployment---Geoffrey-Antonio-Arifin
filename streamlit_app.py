a67a5e0d85be6b0b6a0bb3e523a8f3b4bb96afe6
GeoffreyAntonio19 <geo.arifin@gmail.com>
2025-03-25 16:04:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,8 @@ with st.expander('**Input Features**'):
   gender = st.selectbox('Gender', ('Male', 'Female'))
   family_history = st.selectbox('Family Member History with Overweight', ('Yes', 'No'))
   favc = st.selectbox('FAVC', ('Yes', 'No'))
+  caec = st.selectbox('CAEC', ('Sometimes', 'Frequently', 'Always', 'No'))
+  smoke = st.selectbox('SMOKE', ('Yes', 'No'))
+  scc = st.selectbox('SCC', ('Yes', 'No'))
+  calc = st.selectbox('CALC', ('Yes', 'No'))
+  mtrans = st.selectbox('MTRANS', ('Yes', 'No'))
