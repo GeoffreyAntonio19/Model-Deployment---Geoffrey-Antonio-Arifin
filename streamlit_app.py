3df5965f6101b46718d47a07a9ab8ea3bd8a50f1
GeoffreyAntonio19 <geo.arifin@gmail.com>
2025-03-25 15:42:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ from sklearn.ensemble import RandomForestClassifier
 
 st.title('Obesity Classification Prediction')
 st.info('This application can predict your obesity class based on some parameters!')
+
+with st.expander('1. Data'):
+  st.write('This is a raw data')
+  df = pd.read_csv('ObesityDataSet_raw_and_data_sinthetic.csv')
+  df
